421b098f517f5b6c2fe99abf924c6e20234998a2
Sejal Saraogi <sejalsaraogi260@gmail.com>
2020-10-04 16:50:53 +0530

    list again

---
+++ list/src/countryReport.ts
@@ -0,0 +1,14 @@
+export interface countryReports{
+    country:string;
+    cases:number;
+    todayCases:number;
+    deaths:string;
+    todayDeaths:string;
+    recovered:number;
+    active:number;
+    critical:string;
+    casesPerOneMillion:number;
+    deathsPerOneMillion:number;
+    totalTests:string;
+    testsPerOneMillion:string;
+}
